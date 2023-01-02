49fa81f52370148a0827cbd55c3854b12d1fe539
Milad Dev <milad.beigi0098@gmail.com>
2023-01-02 21:21:54 +0330

    createe Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,95 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
-import React from "react";
-
-const StyledToolbar=styled(Toolbar)({
-    display:"flex",
-    justifyContent:"space-between"
-})
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import "../assets/images/userr.png";
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+const USerBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 export const Navbar = () => {
+  const [open,setOpen]=useState(false)
   return (
     <AppBar position="sticky">
-      <StyledToolbar><Typography variant="h6">LAMA DEV </Typography></StyledToolbar>
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          LAMA DEV
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sc={{ width: 30, height: 30 }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU"
+            onClick={e=>setOpen(true)}
+          />
+        </Icons>
+        <USerBox onClick={e=>setOpen(true)}>
+          <Avatar
+            sc={{ width: 30, height: 30 }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6v-Quj0rUbKfkYkO5xry7QsyV_3dNemjlbw&usqp=CAU"
+          />
+          <Typography variant="span">Milad</Typography>
+        </USerBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal:"left",
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
